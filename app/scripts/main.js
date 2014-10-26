f24e3ea8abba4fd60c074c7f3178ca0e56c0d56f
Mikey McOrmond <michael.mcormond@gmail.com>
2014-10-25 21:10:51 -0400

    Adds conditional image CSS rules, image files.

---
+++ app/scripts/main.js
@@ -0,0 +1,4 @@
+// domready(function() {
+// 	// document.getElementsByTagName('body').className = document.getElementsByTagName('body').className.replace(/\btransparent-onload\b/,'');
+// 	// document.getElementsByTagName('body').classList.remove('transparent-onload');
+// })
